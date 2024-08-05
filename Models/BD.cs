01db3563236ad4c12367ae428ec7e2d31bf8f59d
MICHELLE SOFIA GARBER <48521612@ort>
2024-08-05 10:34:17 -0300

    Dia 1

---
+++ Models/BD.cs
@@ -0,0 +1,12 @@
+using System.Data.SqlClient;
+using Dapper;
+public class BD{
+    private static string _connectionString = @"Server=localhost;
+    Database=JJOO_Szwarcbort_Garber;Trusted_Connection=True";
+    public static void AgregarDeportista(Deportista dep){
+    string SQL = "INSERT INTO Deportistas( Nombre, Apellido, FechaNacimiento, Foto, IdPais, IdDeporte) VALUES ( @pNombre, @pApellido, @pFechaNacimiento, @pFoto, @pIdPais, @pIdDeporte)";
+    using(SqlConnection db = new SqlConnection(_connectionString)){
+        db.Execute(SQL, new{pNombre = dep.Nombre, pIdDeporte = dep.IdDeporte, pApellido =dep.Apellido, pFechaNacimiento = dep.FechaNacimiento, pFoto = dep.Foto, pIdPais = dep.IdPais});
+    }
+    }
+}
